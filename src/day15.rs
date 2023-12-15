0265416ae69f2f27b2c99b8c2b17c28b36b5ec6b
Einar Tryggvi Leifsson <einartryggvi@gmail.com>
2023-12-15 09:19:37 +0000

    Day 15 Part 1

--- src/day15.rs
+++ src/day15.rs
@@ -1,14 +1,32 @@
 pub mod part1 {
     use std::fs;
 
-    fn do_stuff(contents: &str) -> i32 {
-        contents.len() as i32
+    fn hash(string: &str) -> usize {
+        let mut result = 0;
+
+        for char in string.chars() {
+            result += char as usize;
+            result *= 17;
+            result %= 256;
+        }
+
+        result
+    }
+
+    fn hash_sum(contents: &str) -> usize {
+        let mut sum = 0;
+
+        for string in contents.split(',') {
+            sum += hash(string);
+        }
+
+        sum
     }
 
     pub fn run() {
         let contents = fs::read_to_string("inputs/day15.txt").expect("File not found");
 
-        let result = do_stuff(&contents);
+        let result = hash_sum(&contents);
 
         println!("Day 15 Part 1: {}", result);
     }
@@ -18,8 +36,16 @@ pub mod part1 {
         use super::*;
 
         #[test]
-        fn test_day15_part1() {
-            assert_eq!(do_stuff(""), 0);
+        fn test_day15_part1a() {
+            assert_eq!(hash_sum("HASH"), 52);
+        }
+
+        #[test]
+        fn test_day15_part1b() {
+            assert_eq!(
+                hash_sum("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"),
+                1320
+            );
         }
     }
 }
